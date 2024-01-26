e4618bf79e7f56db61b4d12ea286eddd1e3f4bfc
EliGandin <git>
2024-01-26 11:28:34 +0200

    updated styling

--- frontend/src/ui/Sidebar.tsx
+++ frontend/src/ui/Sidebar.tsx
@@ -6,8 +6,8 @@ const SideBar = () => {
       className="absolute top-10 z-40 flex h-[901px] w-96 flex-col items-center justify-between space-y-4 rounded-br-lg rounded-tr-lg border border-neutral-900 
     bg-neutral-600 p-14 text-neutral-100"
     >
-      <img className="gap-4" src="../../public/logo-color.svg" alt="trackie" />{" "}
-      //TODO: Style
+      <img className="gap-4" src="/logo-color.svg" alt="trackie" /> //TODO:
+      Style
       {/* sidebar placeholder */}
       <NewPostForm />
       <button className="flex w-[32px] items-center justify-center gap-2 rounded-full border border-solid bg-neutral-600 pb-1 text-lg font-bold">
