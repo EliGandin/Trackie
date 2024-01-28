69ff0d55d08d9ef1f5c84954365cff04a9b6bd00
EliGandin <git>
2024-01-28 15:28:34 +0200

    improved sidebar-slice

--- frontend/src/ui/sidebarSlice.ts
+++ frontend/src/ui/sidebarSlice.ts
@@ -3,14 +3,24 @@ import { RootState } from "../store";
 
 const initialState = {
   isExpanded: false,
+  content: "",
 };
 
 const sidebarSlice = createSlice({
   name: "sidebar",
   initialState,
   reducers: {
-    toggleVisible(state, action) {
-      state.isExpanded = action.payload;
+    toggleVisible(state) {
+      state.isExpanded = !state.isExpanded;
+    },
+
+    renderContent(state, action) {
+      if (state.content === action.payload.content) {
+        sidebarSlice.caseReducers.toggleVisible(state);
+      } else {
+        state.isExpanded = true;
+        state.content = action.payload.content;
+      }
     },
   },
 });
@@ -19,6 +29,10 @@ export const isSideBarExpanded = (state: RootState) => {
   return state.sidebar.isExpanded;
 };
 
-export const { toggleVisible } = sidebarSlice.actions;
+export const getContent = (state: RootState) => {
+  return state.sidebar.content;
+};
+
+export const { toggleVisible, renderContent } = sidebarSlice.actions;
 
 export default sidebarSlice.reducer;
