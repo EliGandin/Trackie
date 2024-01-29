4649fbc3af80093d1a0249de95e6c4cd2f20dacd
EliGandin <git>
2024-01-29 18:16:38 +0200

    set up new post form

--- frontend/src/features/NewPost/NewPost.tsx
+++ frontend/src/features/NewPost/NewPost.tsx
@@ -1,14 +1,11 @@
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 
 const NewPost = () => {
   const {
     register,
     handleSubmit,
-    // formState: { errors, isSubmitting },
-    getValues,
+    // formState: { isSubmitting },
   } = useForm();
-  const navigate = useNavigate();
 
   //   const onSubmit = async (data: FieldValues) => {
   // const res = await fetch("http://localhost:8000/signup", {
@@ -23,58 +20,41 @@ const NewPost = () => {
   //   };
 
   return (
-    <div className="flex flex-col">
-      <div>
-        {/* <img
-              className="z-10 blur-sm"
-              src="/signup.jpg"
-              alt="background-signup"
-            /> */}
-      </div>
-
+    <div className="flex flex-col text-stone-800">
       <form
-        className=""
+        className="flex flex-col"
         // onSubmit={handleSubmit(onSubmit)}
       >
         <p className="mb-1 text-neutral-300">Location</p>
-        <input placeholder="Tokyo, Japan" {...register("locationName")} />
+        <div className="mb-3 flex">
+          <input
+            className="rounded px-4 py-2"
+            placeholder="Tokyo, Japan"
+            {...register("locationName")}
+          />
 
-        <p>Story</p>
-        <input placeholder="Tell us about this place" {...register("story")} />
+          <button className="ml-2 rounded border border-stone-100 bg-zinc-600 p-2 text-stone-100">
+            Fly To Location
+          </button>
+        </div>
 
-        <button className="absolute bottom-9 w-20 rounded border bg-emerald-500 py-1 text-neutral-100 hover:bg-emerald-400 disabled:bg-neutral-600 disabled:text-neutral-400">
-          Upload up to 10 photos
+        <p className="mb-1 text-neutral-300">Story</p>
+        <textarea
+          className="h-32 rounded px-4 py-2 text-start"
+          placeholder="Tell us about this place"
+          {...register("story")}
+        />
+
+        <button
+          type="submit"
+          className="mt-5 w-24 self-center rounded-lg border bg-emerald-500 py-1 text-neutral-100 hover:bg-emerald-600 disabled:bg-neutral-600 disabled:text-neutral-400"
+          // disabled={isSubmitting}
+        >
+          Upload Photos
         </button>
-
-        {/* <ul>
-              {errors.email && (
-                <FieldValueError error={`${errors.email.message}`} />
-              )}
-              {errors.password && (
-                <FieldValueError error={`${errors.password.message}`} />
-              )}
-              {errors.confirmPassword && (
-                <FieldValueError error={`${errors.confirmPassword.message}`} />
-              )}
-            </ul> */}
-
-        {/* <div className="absolute bottom-2 mx-2 flex text-sm text-stone-50">
-              <p className="px-1">Already have an account?</p>
-              <Link
-                to="/login"
-                className="px-2 text-emerald-400 hover:text-emerald-500"
-              >
-                Sign in
-              </Link>
-            </div> */}
-
-        {/* <button
-              type="submit"
-              className="absolute bottom-9 w-20 rounded border bg-emerald-500 py-1 text-neutral-100 hover:bg-emerald-400 disabled:bg-neutral-600 disabled:text-neutral-400"
-              disabled={isSubmitting}
-            >
-              Sign Up
-            </button> */}
+        <p className="mt-2 self-center text-neutral-400">
+          upload up to 3 photos
+        </p>
       </form>
     </div>
   );
