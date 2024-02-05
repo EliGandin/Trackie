03befacdcc0cf4612dc079e28ba052326bba5afd
EliGandin <git>
2024-02-05 10:54:40 +0200

    set up redux-persistor

--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -1,16 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
 import mapSlice from "./slices/mapSlice";
 import sidebarSlice from "./slices/sidebarSlice";
 import userSlice from "./slices/userSlice";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducers = combineReducers({
+  map: mapSlice,
+  sidebar: sidebarSlice,
+  user: userSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    map: mapSlice,
-    sidebar: sidebarSlice,
-    user: userSlice,
-  },
+  reducer: persistedReducer,
 });
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export { store, persistor };
