6c177c304846436128d5daff621dcd600cac9d94
EliGandin <git>
2024-02-20 07:57:56 +0200

    initial work in Autocomplete component

--- frontend/src/features/Search/Search.tsx
+++ frontend/src/features/Search/Search.tsx
@@ -1,5 +1,19 @@
+import { TextField } from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const ops = [{ label: "The Shawshank Redemption", year: 1994 }];
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <Autocomplete
+      className="relative top-20 rounded-lg bg-neutral-300 p-2 focus:text-neutral-300 focus:ring-neutral-600"
+      disablePortal
+      id="search"
+      options={ops}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Location" />}
+    />
+  );
 };
 
 export default Search;
