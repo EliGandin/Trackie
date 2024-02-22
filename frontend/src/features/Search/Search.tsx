95389c3667f0aa69cec54cd8db1759a5cd34c484
EliGandin <git>
2024-02-22 15:25:06 +0200

    styled component

--- frontend/src/features/Search/Search.tsx
+++ frontend/src/features/Search/Search.tsx
@@ -6,12 +6,28 @@ const ops = [{ label: "The Shawshank Redemption", year: 1994 }];
 const Search = () => {
   return (
     <Autocomplete
-      className="relative top-20 rounded-lg bg-neutral-300 p-2 focus:text-neutral-300 focus:ring-neutral-600"
+      className="relative top-20 rounded-lg bg-neutral-300 p-2"
       disablePortal
       id="search"
       options={ops}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Location" />}
+      sx={{
+        width: 300,
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Location"
+          sx={{
+            "& .MuiInputLabel-root.Mui-focused": { color: "black" },
+            "& .MuiOutlinedInput-root.Mui-focused": {
+              "& > fieldset": {
+                borderColor: "#525252",
+                color: "black",
+              },
+            },
+          }}
+        />
+      )}
     />
   );
 };
