ac0f33501f3bb36c15558eb20e7f5400c21f0f38
EliGandin <git>
2024-03-18 15:42:09 +0200

    improved data fetching

--- frontend/src/services/geoLocationServices.ts
+++ frontend/src/services/geoLocationServices.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-export const getCoordinates = async (location: string | null) => {
-  if (location === "") return;
+export const getCoordinates = async (query: string | null) => {
+  if (!query) return;
 
-  const res = await axios.get(
-    `https://us1.locationiq.com/v1/search?key=pk.6966cf1f220a4ad3b6e8209d22b74597&format=json&q=${location}`,
-  );
+  const res = await axios.get(`http://localhost:8000/location/${query}`);
   return res.data;
 };
